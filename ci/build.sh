158f1691c7636a11d924e1001a3e739cbdc00521
Branco <branco.bruyneel@gmail.com>
2022-05-12 21:03:49 +0200

    ci: test build.sh

---
+++ ci/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -ex
+
+CI=true
+web="$(pwd)/web"
+
+# Build the API (using the Rust toolchain + wasm-pack)
+cd $web
+npm install
+wget -qO- https://github.com/thedodd/trunk/releases/download/${VERSION}/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
+
+npm run prod
