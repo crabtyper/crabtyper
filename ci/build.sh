f75303c62072b89737e622d988cd9f5778843642
Branco <branco.bruyneel@gmail.com>
2022-05-13 17:52:36 +0200

    ci

--- ci/build.sh
+++ ci/build.sh
@@ -2,8 +2,6 @@
 
 set -ex
 
-VERSION=0.15.0
-
 # install latest trunk version
 wget -qO- https://github.com/thedodd/trunk/releases/download/${VERSION}/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
 
