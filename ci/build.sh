3c581764760ac7447fd85265ac2e9f5115bffacf
Branco <branco.bruyneel@gmail.com>
2022-05-13 17:49:50 +0200

    ci: fix version

--- ci/build.sh
+++ ci/build.sh
@@ -2,10 +2,12 @@
 
 set -ex
 
+VERSION=0.15.0
+
 # install latest trunk version
-wget -qO- https://github.com/thedodd/trunk/releases/download/latest/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
+wget -qO- https://github.com/thedodd/trunk/releases/download/${VERSION}/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
 
 cd web/
 
 npm install
-npm run build
+npm run prod
