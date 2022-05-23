1c7a936c421981982b6f8aa3e19f462bf89a47c7
Branco <branco.bruyneel@gmail.com>
2022-05-23 19:18:24 +0200

    fix(web): remove wee_alloc

--- web/src/main.rs
+++ web/src/main.rs
@@ -5,10 +5,6 @@ mod utils;
 
 use crate::components::app::App;
 
-// Use `wee_alloc` as the global allocator.
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 fn main() {
     yew::Renderer::<App>::new().render();
 }
