bd9411e0b86cc3fc52b43bd063a2365f4d62ecaa
Branco <branco.bruyneel@gmail.com>
2022-06-16 17:19:11 +0200

    refactor: move crates to crates/

--- ci/build.sh
+++ ci/build.sh
@@ -5,7 +5,7 @@ set -ex
 # # install latest trunk version
 # wget -qO- https://github.com/thedodd/trunk/releases/download/${VERSION}/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
 
-cd web/
+cd crates/web/
 
 npm install
 npm run build
