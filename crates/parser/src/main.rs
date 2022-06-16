bb2a4e16ef044c857d7f8fb171529ad7bea0f034
Branco <branco.bruyneel@gmail.com>
2022-06-16 18:09:14 +0200

    fix: parser

--- crates/parser/src/main.rs
+++ crates/parser/src/main.rs
@@ -5,8 +5,9 @@ use std::io::Write;
 fn main() {
     let data = fs::read_to_string("data.txt").expect("Unable to read file");
 
-    let mut result = data.replace("\n", "\\n");
-    result = result.replace("\t", "\\t");
+    let mut result = data.replace('\n', "\\n");
+    result = result.replace('\t', "\\t");
+    result = result.replace('\"', "\\\"");
 
     let mut file = File::create("output.txt").unwrap();
     file.write_all(result.as_bytes()).unwrap();
